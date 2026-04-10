d765766858c13178743830c94ae648a10107266e
Verifieddanny <dannyclassic56@gmail.com>
2026-04-10 16:20:09 +0100

    Feat: WS URL DYNAMIC

--- src/index.ts
+++ src/index.ts
@@ -64,7 +64,10 @@ const subdomainMiddleware = (
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
 
 app.use(
   (error: CustomError, _req: Request, res: Response, _next: NextFunction) => {
